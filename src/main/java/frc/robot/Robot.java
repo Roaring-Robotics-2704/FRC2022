23b204c80c55fd083bcaff5f54c8f100f3564680
Lily B <43483482+0051200@users.noreply.github.com>
2022-03-19 13:09:52 -0500

    Added Smartdashboard showing winch servo

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.BallActuator_Servo;
@@ -84,6 +85,7 @@ public class Robot extends TimedRobot {
     RobotContainer.m_ballActuator.servoAngle = 180;
     RobotContainer.m_ballActuator.ballServo.setAngle(RobotContainer.m_ballActuator.servoAngle);
     RobotContainer.m_winch.winchServo.setAngle(0);
+    SmartDashboard.putBoolean("Servo Lock Engaged", false);
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
